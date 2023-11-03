24dab60af555abf84bc891c674aad75e31f00a2b
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-03 12:08:21 -0400

    frotnend Friday project Login

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <Footer>
+        <h2>
+        <small>
+        Copyright © Andy
+        </small>
+        </h2>
+        </Footer>
+    )
+}
